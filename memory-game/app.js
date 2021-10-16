72583860a2345d13a9036b397c789578e1cbfac7
Inbar Vernia <inbar.vernia@gmail.com>
2021-10-16 19:41:09 +0100

    Initial styling for .grid in style.css, created array of cards in app.js

---
+++ memory-game/app.js
@@ -0,0 +1,28 @@
+
+// card options (to be re-named later, when images have been made/chosen)
+const cardArray = [
+  {
+    name: 'name',
+    img: 'images/name.png'
+  },
+  {
+    name: 'name',
+    img: 'images/name.png'
+  },
+  {
+    name: 'name',
+    img: 'images/name.png'
+  },
+  {
+    name: 'name',
+    img: 'images/name.png'
+  },
+  {
+    name: 'name',
+    img: 'images/name.png'
+  },
+  {
+    name: 'name',
+    img: 'images/name.png'
+  }
+]
